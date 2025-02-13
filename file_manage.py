1b753e879f2b803f4117a1644c0ae976543ef4e9
Dagem Legesse <dlegess1@terpmail.umd.edu>
2025-02-13 00:32:28 -0500

    Adding file_manage.py and usb_manage.py

---
+++ file_manage.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import psutil
+
+
+def picture_finder():
+    """Utilize glob module to find pictures
+    """
+    return None
+
+def directory_access():
+    """Find the picture directory from where the current working directory. 
+    """
+    return None
+
+def file_finder():
+    """Find all the non picture files
+
+    Returns:
+        _type_: _description_
+    """
+    return None
+
+
+def verify_file():
+    """File verification - hash comparison
+    """
+
+def transfer():
+    """In the name.
+    """
+    return None
+
+
+# def main():
+    
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ usb_manage.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import psutil
+
+
+def usb_detection():
+    """utilize pywin32 for real time usb monitoring
+
+    Returns:
+        _type_: _description_
+    """
+    return None
+
+
+def find_usb_mountpoint():
+    """Find the usb mounpoint
+    """
+    return None
+    
+def find_usb():
+    """Find the usb
+    """
+    return None
+
+
+
+
+
+# def main():
+
+
+
+# if __name__ == "__main__":
+#     main()
